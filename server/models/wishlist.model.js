1b2abe8558e317ff5bd5d42cc0d23f5355b24cc4
unknown <ankit.singhania767@gmail.com>
2021-07-09 15:56:05 +0530

    removed unnecessary code

--- server/models/wishlist.model.js
+++ server/models/wishlist.model.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const wishListScehma = new Schema({
-  productIds: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-    },
-  ],
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+	productIds: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Product'
+		}
+	],
+	userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
-const WishList = mongoose.model("Wishlist", wishListScehma);
+const WishList = mongoose.model('Wishlist', wishListScehma);
 module.exports = { WishList };
